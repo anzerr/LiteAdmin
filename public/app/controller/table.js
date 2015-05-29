f0cb2abe42681916549ccbe9a10c380a23ef4cff
anzerr <ryan.anzer@epitech.eu>
2015-05-29 16:18:04 +0200

    added page system

--- public/app/controller/table.js
+++ public/app/controller/table.js
@@ -6,16 +6,45 @@ var Jinx;
 		$scope.sql = {
 			current: 'SELECT * FROM ' + $routeParams.TName + ';',
 			run: function() {
+				var self = this;
 				base.query($http, this.current, $routeParams.database).success(function(res) {
 					console.log(res);
 					try {
-						$scope.sql.result = res
+						self.result = res
+						self.page = 0;
+						self.max = Math.ceil(res.data.length / self.display);
 					} catch (e) {
 						console.log(e);
 					}
 				});
 			},
 			result: [],
+			display: 25,
+			page: 0,
+			max: 0,
+			setDisplay: function(n) {
+				this.display = n;
+				this.max = (this.result.data.length / this.display);
+			},
+			editTable: function(key) {
+				var elem = this.result.data[key];
+				if (elem.edit == true) {
+					console.log("save to server");
+				}
+				elem.edit = (elem.edit == true) ? false : true;
+			},
+			getNumber: function() {
+				var tmp = [], p = (this.page * this.display);
+				for (var i = p; i < p + this.display; i++) {
+					if (this.result.length == 0 || i < this.result.data.length) {
+						tmp.push(i);
+					}
+				}
+				return (tmp);
+			},
+			range: function(n) {
+				return new Array(n);
+			}
 		}
 		
 		$scope.sql.run();
