32b5478f57bcf713b968e15df41d6610dc59b977
anzerr <ryan.anzer@epitech.eu>
2015-05-29 16:18:49 +0200

    merge

--- public/app/controller/table.js
+++ public/app/controller/table.js
@@ -44,7 +44,30 @@ var Jinx;
 			},
 			range: function(n) {
 				return new Array(n);
-			}
+			},
+			getSize: function() {
+				var size = 0, pow = 1, tmp = 0;
+				while (pow < this.max) {
+					pow = pow * 10;		
+					size += 26 + (8 * ((pow - 1) - tmp));
+					tmp = pow - 1;
+				}
+				return (size);
+			},
+			getPosX: function() {
+				var cur = Math.max(this.page - 5, 0), size = 0, pow = 1, tmp = 0, num = 1;
+				while (pow < cur) {
+					pow = pow * 10;
+					size += (26 + (8 * num)) * (Math.min(pow - 1, cur) - tmp);
+					console.log("p: " + Math.min(pow - 1, cur) + " - " + tmp);
+					tmp = pow - 1;
+					num += 1;
+				}
+				
+				console.log(size +" "+ cur);
+				
+				return (size);
+			},
 		}
 		
 		$scope.sql.run();
